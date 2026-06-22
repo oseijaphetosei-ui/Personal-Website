85d1db9c3a6d61419ed2661ea99f14fcf085f6c3
Japhet795 <jotk2024@mymail.pomona.edu>
2026-06-22 13:34:10 -0700

    test deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [],
-  },
-  turbopack: {
-    root: path.resolve(__dirname),
   },
 };
 
